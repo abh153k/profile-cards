3853178631fe79e9e3a453b00a357100708255f9
Abhisek <abhisek.dutt@gmail.com>
2018-10-11 12:05:43 +0530

    Using jQuery for equal height columns.

--- functions.js
+++ functions.js
@@ -1,6 +1,10 @@
-document.addEventListener('DOMContentLoaded', function(e){
-    // Dirty hack for now
-    console.log('Loaded');
-    const info_card = document.getElementById("info-card");
-    document.querySelector(".card").style.height = info_card.clientHeight;
+jQuery(document).ready(function($) { //noconflict wrapper
+    /* Set cards' height to the max */
+    // From https://stackoverflow.com/a/35342714/450733
+    var heights = $(".card").map(function() {
+        return $(this).height();
+    }).get(),
+    maxHeight = Math.max.apply(null, heights);
+    $(".card").height(maxHeight);
+
 });
