0900bf9f0acfe84ebb9c52c2c0206c88d55599bb
Encode8 <email@encode8.com>
2018-10-11 23:17:24 +0530

    Working on the last card and animation.

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,7 @@
-jQuery(document).ready(function($) { //noconflict wrapper
+imagesLoaded(document, function() { //noconflict wrapper
+    // Remove cover
+    $("#cover").hide();
+
     /* Set cards' height to the max */
     // From https://stackoverflow.com/a/35342714/450733
     var heights = $(".card").map(function() {
@@ -6,5 +9,11 @@ jQuery(document).ready(function($) { //noconflict wrapper
     }).get(),
     maxHeight = Math.max.apply(null, heights);
     $(".card").height(maxHeight);
+
+    // Baffle
+    baffle(".obfus", {
+        characters: "10101",
+        speed: 45
+    }).reveal(1500);
 
 });
