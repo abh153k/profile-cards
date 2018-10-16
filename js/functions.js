d09591e84129405a9352c83b53d380332aabc78b
Abhisek <abhisek.dutt@gmail.com>
2018-10-16 11:54:06 +0530

    Some more refactoring.

--- js/functions.js
+++ js/functions.js
@@ -16,23 +16,20 @@ imagesLoaded(document, function() { //noconflict wrapper
         $(this).on('click', function(e){
             var target_card = $(this).data("target-card");
             console.log("Target is", target_card);
-            // send top to back
             anime({
                 targets: ".card:first-child",
-                rotate: [0,"-15deg",0],
                 translateX: [0,282,0],
                 easing: 'linear',
                 duration:350,
-                elasticity:500,
-                /*complete: function(){
-                    $(".card:first-child").css({"top":"-20px", "left": "-20px"}).appendTo("#stack");
-                }*/
+                complete: function(){
+                    // send top to back
+                    console.log("Appending", $("#stack:first-child"), "to stack...");
+                    $(".card:first-child").appendTo("#stack");
+                    // Bring target card to front
+                    console.log("Prepending", target_card, " to stack...")
+                    $(target_card).prependTo("#stack");
+                }
             })
-            console.log("Appending to stack...");
-            $("#stack:first-child").appendTo("#stack");
-            // Bring target card to front
-            console.log("Prepending", target_card, " to stack...")
-            $(target_card).prependTo("#stack");
         });
     })
 
@@ -47,7 +44,7 @@ imagesLoaded(document, function() { //noconflict wrapper
         }
     }).add({
         targets: "#info h2",
-        opacity: [0,1],
+        scale: [0,1],
         translateX: ["24px",0],
     }).add({
         targets: "#bio",
@@ -61,5 +58,20 @@ imagesLoaded(document, function() { //noconflict wrapper
             return Math.sin(i*Math.PI/2);
         }
     });
+
+
+    $('input, textarea').focus(function(){
+        $(this).parents('.form-group').addClass('focused');
+    });
+
+    $('input, textarea').blur(function(){
+        var inputValue = $(this).val();
+        if ( inputValue == "" ) {
+            $(this).removeClass('filled');
+            $(this).parents('.form-group').removeClass('focused');  
+        } else {
+            $(this).addClass('filled');
+        }
+    })
 
 });
